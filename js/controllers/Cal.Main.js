bf3a7c69e32fe3578ce3cad7c9bc8001268a5d99
Z. Feuerborn <risingfish@gmail.com>
2012-04-09 21:57:24 -0600

    got the previous months buffer days showing up correctly.

--- js/controllers/Cal.Main.js
+++ js/controllers/Cal.Main.js
@@ -2,34 +2,55 @@
  * A Calendar Day
  */
 Cal.Main = function(opts) {
-    var currentDate = new Date();
-    this.month = (opts.month) ? parseInt(opts.month) : currentDate.getMonth(); // Can't use the normal || operator because 0 evals to false
-    this.year = opts.year || currentDate.getFullYear();
+    var currentDate = new Date(),
+        month = (opts.month) ? parseInt(opts.month) : currentDate.getMonth(), // Can't use the normal || operator because 0 evals to false
+        year = opts.year || currentDate.getFullYear();
     
-    this.menu = {};
+    // Models
+    this.options = opts;
+    this.cDate = moment([year, month]);
+    this.previousMonth = {};
     this.currentMonth = {};
+    this.nextMonth = {};
+
+    // Views
+    this.menu = {};
     this.monthView = {};
         
     this.initialize = function() {
-        
-        var cDate = moment([this.year, this.month]),
-            pDate = moment(cDate).add('M', -1),
-            nDate = moment(cDate).add('M', 1);
-            
-            console.log(pDate, cDate, nDate);
+        var monthViewModel;
         
         // Setting up the menu bar
         this.menu = new Cal.MenuBarView({
-            model: new Cal.MenuBar({'year': this.year, 'month': this.month})
+            model: new Cal.MenuBar({'year': this.cDate.year(), 'month': this.cDate.month()})
         });
         
         // Setting up the main calendar viewport
-        this.currentMonth = new Cal.CalendarMonth({'year': this.year, 'month': this.month}),
-        this.monthView = new Cal.CalendarMonthView({'model': this.currentMonth, 'className': 'calendar-day-view'});
+        this.setCurrentDate(this.cDate.year(), this.cDate.month());
+        
+        monthViewModel = {
+            weekStartNum: moment([this.cDate.year(), this.cDate.month(), 1]).day(),
+            previous: this.previousMonth,
+            current: this.currentMonth,
+            next: this.nextMonth
+        };
+        
+        this.monthView = new Cal.CalendarMonthView({'model': monthViewModel, 'className': 'calendar-day-view'});
         
         // Appending the new controls to the page
         $(opts.target).append(this.menu.render().el);
         $(opts.target).append(this.monthView.render().el);
+    }
+    
+    this.setCurrentDate = function(year, month) {
+        this.cDate = moment([year, month]);
+
+        var pDate = moment(this.cDate).add('M', -1),
+            nDate = moment(this.cDate).add('M', 1);
+            
+        this.previousMonth = new Cal.CalendarMonth({'year': this.cDate.year(), 'month': this.cDate.month()});
+        this.currentMonth = new Cal.CalendarMonth({'year': pDate.year(), 'month': pDate.month()});
+        this.nextMonth = new Cal.CalendarMonth({'year': nDate.year(), 'month': nDate.month()});
     }
 
     this.initialize();
--- js/views/Cal.CalendarMonthView.js
+++ js/views/Cal.CalendarMonthView.js
@@ -5,10 +5,31 @@ Cal.CalendarMonthView = Backbone.View.extend({
     },
 
     render: function (opts) {
-        console.log("here", this, opts)
-        var self = this, renDay = {};
-        // Render each day.
-        _(this.model.get('dayCollection').models).each(function (item) {
+        var self = this, renDay = {}, preBuffer = new Cal.DayList(), preLen = 1;
+
+        // Setting up the comparator for the previous month days. This will make
+        // sure the days are sorted properly (oldest date to newer)
+        preBuffer.comparator = function (day1, day2) {
+            return (day1.get('dayDate') > day2.get('dayDate')) ? 1 : -1;
+        }
+
+        // Pop the required number of days off the previous month day collection
+        // so that we can put the proper number of buffer days in.
+        for (preLen = 1; preLen < this.model.weekStartNum; preLen++) {
+           preBuffer.add(this.model.previous.get('dayCollection').models.pop());
+        }
+        
+
+        // Render the previous month buffer days..
+        _(preBuffer.models).each(function (item) {
+            renDay = new Cal.CalendarDayView({
+                model: item
+            });
+            $(self.el).append(renDay.render().el);
+        });
+
+        // Render the current month buffer days.
+        _(this.model.current.get('dayCollection').models).each(function (item) {
             renDay = new Cal.CalendarDayView({
                 model: item
             });
